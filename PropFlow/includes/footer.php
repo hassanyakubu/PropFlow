faa38606de02a9afe4008e69d1f67708d2343ece
Hassan Maltiti Yakubu <146729310+hassanyakubu@users.noreply.github.com>
2026-05-03 21:40:29 +0000

    Delete PropFlow directory

--- PropFlow/includes/footer.php
+++
@@ -1,5 +0,0 @@
-<footer class="main-footer">
-    <div class="container">
-        <p>&copy; <?php echo date('Y'); ?> PropFlow. Use of Institutional Helper Files Acknowledged.</p>
-    </div>
-</footer>
--- PropFlow/public/logout.php
+++
@@ -1,9 +0,0 @@
-<?php
-require_once '../settings/core.php';
-
-// Destroy session
-session_destroy();
-
-// Redirect to home
-header("Location: index.php");
-exit();
